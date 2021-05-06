b4376b390b2e85413b6f42f226115b5351a5a7bb
CamilleGAR <58259424+CamilleGAR@users.noreply.github.com>
2021-05-06 19:21:41 +0200

    Add files via upload

--- code/IA.py
+++ code/IA.py
@@ -32,10 +32,15 @@ class IaLearner:
         self.data = functools.reduce(lambda x,y: np.append(x, y, axis = 0),
                                     files_npy)
         
-        #Ici pour prendre connaissance des variables qui sont utiles. Doivent etre setup
-        self.keyboard_inputs_str = None  #Doit etre setup
-        self.keyboard_inputs_str = None  #Doit etre setup
+        #Ici pour prendre connaissance des variables qui sont utiles. 
+        #Doivent etre setup au cours du programme
+        self.keyboard_inputs_str = None
+        self.keyboard_inputs_int = None  
+        self.keyboard_columns_df = None
+        self.df_min = None
+        self.df_max = None
       
+        
     def setup_inputs_keyboard(self, *inputs):
         """"Setup les inputs claviers (outputs de notre reseaux)"""
         possible_inputs = [['gauche'],['haut'],['droite'],['bas'],
@@ -43,14 +48,15 @@ class IaLearner:
                           []]
         self.keyboard_inputs_str = [possible_inputs[i] for i in inputs]
         self.keyboard_inputs_int = inputs
+        self.keyboard_columns_df = [f'keyboard_inputs{i}' for i in inputs]
         
         
     def delete_keyboard_inputs(self, dataframe):
         """Delete les inputs clavier non utilises dans les dataframes"""
         
-        possible_inputs = [i for i in range(9)]
-        inputs_a_delete = [i for i in possible_inputs if i not in self.keyboard_inputs_int]
-        df = dataframe.drop(columns=inputs_a_delete)
+        actual_inputs = [column for column in dataframe if 'keyboard_inputs' in column]
+        columns_a_delete = [column for column in actual_inputs if column not in self.keyboard_columns_df]
+        df = dataframe.drop(columns=columns_a_delete)
         
         return df
         
@@ -67,29 +73,24 @@ class IaLearner:
     def normalize(self, df, training_data = False):
         """Normalize la data"""
         
-        columns_data = [column for column in df 
-                        if ('capteur' in column or 'vitesse' in column)]
-        columns_boolean = [column for column in df if column not in columns_data]
-        
         #Si c'est le training set, on setup pour normalizer le reste
         if training_data:
-            self.df_min = df[columns_data].min()
-            self.df_max = df[columns_data].max()
+            self.df_min = df.min()
+            self.df_max = df.max()
             
-        #Partie data normalizee
-        df_data_norm = df[columns_data]-self.df_min/(self.df_max-self.df_min)
-                        
-        #Partie boolean inchangee
-        df_boolean = df[columns_boolean]
-        
-        #On concatene la data normalize et les bools
-        df_normalized =  df = pd.concat([df_data_norm, df_boolean], axis=1, join="inner")
+        #dataframe normalized
+        df_normalized = (df-self.df_min)/(self.df_max-self.df_min)
         
         return df_normalized
-            
-            
-            
-        
+    
+    
+    def get_attributs_predict(self):
+        """Donne les elements importants pour les predictions"""
+    
+        return {'model': self.model,
+                'keyboard_inputs_str': self.keyboard_inputs_str, 
+                'df_min': self.df_min,
+                'df_max': self.df_max}
     
     def setup_data(self, *combinaisons_keyboard):
         """Shuffle, normalize, repartie, ... la data
@@ -129,37 +130,27 @@ class IaLearner:
         df_equilibre = pd.concat(df.loc[df_first_shuffle[f'keyboard_inputs{i}'] == True][:int(min_inputs*0.6)]
                        for i in combinaisons_keyboard)
         
+        #On shuffle pour avoir un ensemble homogene
         df_shuffled = df_equilibre.sample(frac = 1)
-        
-        
-        return df_shuffled
-        
-        
-        #On supprime les inputs clavier qu'on utilise pas dans ce training
-        df_y_train = self.delete_keyboard_inputs(df_y_train)
-        df_y_test = self.delete_keyboard_inputs(df_y_test)
-        
-        np.take(data,
-                np.random.permutation(data.shape[0]),
-                axis=0,
-                out=data);
-        
-        capteurs = data[:,0]
-        vitesse = data[:,1]
-        keyboard_inputs = data[:,2]
-        
-        #repartition de la data
-        X_train, X_test, y_train, y_test = train_test_split(capteurs + vitesse, keyboard_inputs, test_size=0.2)
-        
-        #Transformation en dataframe
-        df_X_train = pd.DataFrame(pd.DataFrame(X_train)[0].to_list())
-        df_X_test = pd.DataFrame(pd.DataFrame(X_test)[0].to_list())
-        df_y_train = pd.DataFrame(pd.DataFrame(y_train)[0].to_list())
-        df_y_test = pd.DataFrame(pd.DataFrame(y_test)[0].to_list())
-        
 
+        #On supprime les inputs clavier qu'on utilise pas dans ce training
+        df_deleted = self.delete_keyboard_inputs(df_shuffled)
+
+        #On recupere le nom des colonnes
+        columns_data = [column for column in df_deleted
+                        if ('capteur' in column or 'vitesse' in column)]
+        columns_boolean = [column for column in df_deleted if column not in columns_data]
         
-        return df_X_train, df_X_test, df_y_train, df_y_test
+        #On repartie en dataset de training et de test
+        X_train, X_test, y_train, y_test = train_test_split(df_deleted[columns_data],    #Inputs
+                                                            df_deleted[columns_boolean], #Outputs
+                                                            test_size=0.2)               #Repartition
+        
+        #On normalize les valeurs.
+        X_train = self.normalize(X_train, training_data = True)
+        X_test = self.normalize(X_test)
+        
+        return X_train, X_test, y_train, y_test
         
     
     def train(self):
@@ -203,9 +194,11 @@ class IaLearner:
         
         
 class IaPlayer:
-    def __init__(self, model, keyboard_str):
-        self.keyboard_str = keyboard_str
+    def __init__(self, model, keyboard_inputs_str, df_min, df_max):
+        self.keyboard_inputs_str = keyboard_inputs_str
         self.model= model
+        self.df_min = df_min
+        self.df_max = df_max
         self.image_acquisition = ImageAcquisition()
         self.preprocessor = Preprocessor()
         
@@ -222,7 +215,9 @@ class IaPlayer:
             screen = self.image_acquisition.take_entire_screenshot()
             capteurs_et_vitesse = self.preprocessor.preprocessing_frame_type0([screen, None])
             capteurs_et_vitesse = capteurs_et_vitesse[0,0] + capteurs_et_vitesse[0,1]
-            keys = self.keyboard_str[np.argmax(self.model.predict([capteurs_et_vitesse]))]
+            capteurs_et_vitesse_normalized = (capteurs_et_vitesse-self.df_min)/(self.df_max-self.df_min)
+            num_input = np.argmax(self.model.predict([capteurs_et_vitesse_normalized.to_list()]))
+            keys = self.keyboard_inputs_str[num_input]
             print(keys)
             for key in ['haut', 'bas', 'gauche', 'droite']:
                 if key in keys:
